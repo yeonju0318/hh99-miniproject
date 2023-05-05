66d7d405e62c8bd6980c428bd532cce78b135dd0
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-05-05 17:12:43 +0900

    Design: Design navBar
    네비게이션 바를 담는 컨테이너 추가

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Container from "./Container";
+import Logo from "./Logo";
+import NavMain from "./NavMain";
+import UserMenu from "./UserMenu";
 
 function Navbar() {
-  return <div className=" text-5xl ">Navbardd</div>;
+  return (
+    <>
+      <div className="fixed w-full bg-white z-10 shado-sm">
+        <div className="py-4 border-b-[1px]">
+          <Container />
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0 ">
+            <Logo />
+            <NavMain />
+            <UserMenu />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
