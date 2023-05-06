2b75aee98beb33fa1841648bb7a44c4725efad4e
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-05-06 10:36:36 +0900

    Add: Add categories under navbar
    네비게이션 바 하단에 유저가 버튼을 눌렀을 때 필터링할 수 있는 버튼 생성

---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Container from "./Container";
+
+function Categories() {
+  return (
+    <Container>
+      <div className="py-1 flex flex-row items-center justify-center overflow-x-auto gap-10">
+        <button>기분</button>
+        <button>날씨</button>
+        <button>장르</button>
+      </div>
+    </Container>
+  );
+}
+
+export default Categories;
---
+++ src/components/CategoryBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CategoryBox({icon, label, selected}) {
+  return (
+    <div>CategoryBox</div>
+  )
+}
+
+export default CategoryBox
