9ea7caf5e8ac2ee48ddffe91d1bee507b5e31073
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-05-06 15:46:51 +0900

    Feat: Add navigate function when press logo
    로고를 누르면 홈으로 가는 기능 추가

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Logo() {
+  const navigate = useNavigate()
   return (
-    <img alt="Logo" className='hidden md:block cursor-pointer' height="100" width="100" src={process.env.PUBLIC_URL + '/imgs/logo.png'} />
+    <img 
+    onClick={() => navigate('/')}
+    alt="Logo" className='hidden md:block cursor-pointer' height="100" width="100" src={process.env.PUBLIC_URL + '/imgs/logo.png'} />
   )
 }
 
