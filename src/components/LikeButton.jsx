37d0dafedccad0f44bad5c6e16b7706fb5a71721
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-05-06 15:46:18 +0900

    Feat: Add function when user press like button
    유저가 좋아요 버튼을 누르면 하트가 차게 했음.
    후에 게시글을 올리고, 좋아요를 누르면 자신이 좋아요 한 목록을 볼 수 있는 페이지를 만들 예정.

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import AuthContext from "../hooks/useCurrentUser";
+import useLoginModal from "../hooks/useLoginModal";
+
+
+
 
 function LikeButton() {
+  const {auth, setAuth } = useContext(AuthContext);
+
+  const [hasLiked,setHasLiked] = useState(false);
+  const toggleLike = () => {
+    setHasLiked(!hasLiked)
+  }
+
   return (
-    <div className="relative hover:opacity-80 transiton cursor-pointer">
+    <div 
+    onClick={toggleLike}
+    className="relative hover:opacity-80 transiton cursor-pointer">
       <AiOutlineHeart
         size={28}
         className="fill-white absolute -top-[2px] -right-[2px]"
       />
-      <AiFillHeart size={24} className="fill-neutral-500/70" />
+      <AiFillHeart size={24} className={
+        hasLiked? 'fill-rose-500' : 'fill-neutral-500/70'
+      }/>
     </div>
   );
 }
