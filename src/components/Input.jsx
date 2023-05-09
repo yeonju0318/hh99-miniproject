be900403750b78538009250620443af0a6ab5c07
Yeonju <cuteyonju@naver.com>
2023-05-10 01:16:42 +0900

    Feat: Gpt에게 질문하는 기능 구현
    - 추가적으로 inputtext에 값을 넣고도 Send가 되기는 함..
    -Message컴포넌트에서 받아온 data를 출력해야해서 전역으로 관리함

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,58 @@
 import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import useFeelTag from "../hooks/useFeelTag";
+import useGenreTag from "../hooks/useGenreTag";
+import useWeatherTag from "../hooks/useWeatherTag";
+import Cookies from "js-cookie";
+import useAnswerGpt from "../hooks/useAnswerGpt";
+import useInput from "../hooks/useInput";
 
 function Input() {
+  // const [inputText, setInputText] = useState("");
+  // const [isQuestionbyButton, setIsQuestionbyButton] = useState(false);
+
+  const feelTag = useFeelTag();
+  const GenreTag = useGenreTag();
+  const WeatherTag = useWeatherTag();
+  const { setAnswerGpt } = useAnswerGpt();
+  const { inputText, setInputText } = useInput();
+
+  const onSendMessage = async () => {
+    const message = {
+      question: `${feelTag.feelTag.text}${GenreTag.GenreTag.text}${WeatherTag.WeatherTag.text}${inputText}`,
+    };
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_SERVER_URL}/chat-gpt/question`,
+        message,
+        {
+          headers: {
+            Access_Token: `Bearer ${Cookies.get("auth")}`,
+          },
+        }
+      );
+      console.log(response.data);
+      setAnswerGpt(response.data.data.answer);
+      // setInputText("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onInputChangeHandler = (e) => {
+    setInputText(e.target.value);
+  };
+
   return (
     <div className="input">
-      <input type="text" placeholder="Type something..." />
+      <input
+        type="text"
+        placeholder="Type something..."
+        onChange={onInputChangeHandler}
+      />
       <div className="send">
-        <button>Send</button>
+        <button onClick={onSendMessage}>Send</button>
       </div>
     </div>
   );
