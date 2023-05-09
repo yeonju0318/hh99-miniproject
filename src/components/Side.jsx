588da9ed9059c0aa0d225066d038b8d96025346b
Yeonju <cuteyonju@naver.com>
2023-05-10 01:18:40 +0900

    Feat: 작성된 내용을 공유하기
    - API명세서 참고해서 코드를 써보긴 했는데 answer값을 어떻게 가져와야할지? -수정중

--- src/components/Side.jsx
+++ src/components/Side.jsx
@@ -1,24 +1,54 @@
 import React from "react";
 import Sidenav from "./Sidenav";
-// import Search from "./Search";
 import Select from "./Select";
-import axios from "axios";
 import useFeelTag from "../hooks/useFeelTag";
+import useGenreTag from "../hooks/useGenreTag";
+import useWeatherTag from "../hooks/useWeatherTag";
+import useAnswerGpt from "../hooks/useAnswerGpt";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 const Side = () => {
-  const question = {
-    question:
-      "나는 오늘 슬퍼. 오늘은 비가 많이 왔어. 클래식 음악을 듣고싶은데 몇개 추천해줘.",
+  const today = {
+    Feel: {
+      id: 1,
+      body: "오늘의 기분은?",
+      tag: ["기쁨", "슬픔", "분노", "불안", "사랑", "기대"],
+    },
+    Weather: {
+      id: 2,
+      body: "오늘의 날씨는?",
+      tag: ["맑음", "흐림", "비", "눈", "폭풍"],
+    },
+    Genre: {
+      id: 3,
+      body: "듣고 싶은 노래는?",
+      tag: ["K-pop", "Pop", "발라드", "힙합", "클래식", "자연"],
+    },
   };
 
-  const onQuestionHandler = async () => {
+  const feelTag = useFeelTag();
+  const GenreTag = useGenreTag();
+  const WeatherTag = useWeatherTag();
+  const { setAnswerGpt } = useAnswerGpt();
+
+  const onShareButton = async () => {
+    const message = {
+      question: `${feelTag.feelTag.text}${GenreTag.GenreTag.text}${WeatherTag.WeatherTag.text}`,
+      answer: "",
+      feelTag: `${feelTag.feelTag}`,
+      weatherTag: `${WeatherTag.WeatherTag}`,
+      genreTag: `${GenreTag.GenreTag.text}`,
+      startPoint: "",
+      endPoint: "",
+    };
     try {
       const response = await axios.post(
-        `${process.env.REACT_APP_SERVER_URL}/chat-gpt/question`,
-        question,
+        `${process.env.REACT_APP_SERVER_URL}/post`,
+        message,
         {
           headers: {
-            Access_Token: `Bearer`,
+            Access_Token: `Bearer ${Cookies.get("auth")}`,
           },
         }
       );
@@ -28,37 +58,17 @@ const Side = () => {
     }
   };
 
-
-
-
-  const today = [
-    {
-      id: 1,
-      body: "오늘의 기분은?",
-      tag: ["기쁨", "슬픔", "분노", "불안", "사랑", "기대"],
-    },
-    {
-      id: 2,
-      body: "오늘의 날씨는?",
-      tag: ["맑음", "흐림", "비", "눈", "폭풍"],
-    },
-    {
-      id: 3,
-      body: "듣고 싶은 노래는?",
-      tag: ["K-pop", "Pop", "발라드", "힙합", "클래식", "자연"],
-    },
-  ];
-
   return (
-    <div className="sidebar">
-      <Sidenav />
-      {/* <Search /> */}
-      {today.map((item) => (
-        <Select key={item.id} today={item} />
-      ))}
-      <div className="flex justify-around">
-        <button onClick={onQuestionHandler}>질문하기</button>
-        <button>올리기</button>
+    <div className="flex flex-col justify-between items-center">
+      <div className="sidebar">
+        <Sidenav />
+        {/* <Search /> */}
+        {Object.values(today).map((today) => (
+          <Select key={today.id} item={today} />
+        ))}
+      </div>
+      <div>
+        <button>공유하기</button>
       </div>
     </div>
   );
